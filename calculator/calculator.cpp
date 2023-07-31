bc11d04f10299c18ce1dc4aca9b32f2e9a5fdddf
1s2h3r4i5t6u <123437071+1s2h3r4i5t6u@users.noreply.github.com>
2023-07-31 20:40:43 +0530

    Add files via upload

---
+++ calculator/calculator.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Welcome to the Basic Calculator!" << endl;
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    double result;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Error: Division by zero is not allowed!" << endl;
+                return 1; // Exit with an error code
+            }
+            break;
+        default:
+            cout << "Invalid operator. Please enter a valid operator (+, -, *, /)." << endl;
+            return 1; // Exit with an error code
+    }
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
